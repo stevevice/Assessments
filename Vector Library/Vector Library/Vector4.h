02790d3208d1bf39d6d097d79578f4c9b61fdab4
stevevice <steve_alexanders@bellsouth.net>
2015-12-01 18:01:11 -0600

    Fourth Push
    adding vector 4

---
+++ Vector Library/Vector Library/Vector4.h
@@ -0,0 +1,29 @@
+#ifndef _VECTOR4_H_
+#define _VECTOR4_H_
+
+class Vector4
+{
+private:
+
+
+public:
+	int R, G, B, A;
+
+	Vector4();
+	~Vector4();
+};
+
+Vector4::Vector4()
+{
+
+}
+
+
+Vector4::~Vector4()
+{
+
+}
+
+
+
+#endif //_VECTOR4_H_
